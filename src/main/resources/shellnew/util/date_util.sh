a908098adb1a756bd5922019c4b5ee650881994c
kangxiongwei3 <kangxiongwei3@le.com>
2017-02-23 11:46:01 +0800

    reviewer:kangxiongwei3 id:0 增加遍历每一天的日期工具函数

--- src/main/resources/shellnew/util/date_util.sh
+++ src/main/resources/shellnew/util/date_util.sh
@@ -11,8 +11,22 @@ function getYesterday {
 function addDays {
     if [ $# != 2 ]; then
         echo "必须输入两个参数，第一个为日期，第二个为增加多少天"
-        exit 1
+        return 1
     fi
     date=`date -d "+$2 days $1" +%Y%m%d`
     echo ${date}
+}
+
+#执行某段时间范围内的指定脚本，日期格式yyyyMMdd
+function execute {
+    if [ $# != 3 ]; then
+        echo "必须传入三个参数[startDate,endDate,shellFile]"
+    fi
+    date=`date -d "+0 day $1" +"%Y%m%d"`
+    while [[ ${date} < $2 ]];
+    do
+        echo "开始执行${date}日的脚本"
+        sh $3 ${date}
+        date=`date -d "+1 day ${date}" +"%Y%m%d"`
+    done
 }
