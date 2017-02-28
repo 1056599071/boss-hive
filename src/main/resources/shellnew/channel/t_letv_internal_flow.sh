e1e7d49b20f01edc7d8a1edd7770e1214fb8513f
kangxiongwei3 <kangxiongwei3@le.com>
2017-02-28 20:34:13 +0800

    reviewer:kangxiongwei3 id:0 增加jssdk上报的ref信息到站内渠道流量

--- src/main/resources/shellnew/channel/t_letv_internal_flow.sh
+++ src/main/resources/shellnew/channel/t_letv_internal_flow.sh
@@ -39,7 +39,7 @@ function httpRef {
         terminalName="PC端"
     fi
     if [ ${terminal} -eq 113 ]; then
-        product=" and product = 0 and p2 in ('04', '05', '06') and cur_url != '-' and cur_url != ''"
+        product=" dt = '${yesterday}' and product = 0 and p2 in ('04', '05', '06') and cur_url != '-' and cur_url != ''"
         terminalName="M站"
     fi
 
@@ -68,12 +68,36 @@ function httpRef {
 #查询JsSdk上报的ref流量
 function jssdkRef {
     terminal=$1
+    if [ ${terminal} -eq 112 ]; then
+        product=" dt = '${yesterday}' and platform = 0 "
+    fi
+    if [ ${terminal} -eq 112 ]; then
+        product=" dt = '${yesterday}' and platform IN (1, 2)"
+    fi
+    #查询ref
+    sql1="select uid, splitUrl(cur_url) as query, eid from dwb.dwb_megatron_pv_hour where ${product}"
+    sql2="select m.uid, m.query['ref'] as ref, m.eid from (${sql1}) m
+     where m.query['ref'] is not null and m.query['ref'] not like '%http:%' and m.query['ref'] != 'click'"
+    #查询订单
+    sql3="select userid, money, neworxufei from dm_boss.t_new_order_4_data
+     where dt = '${yesterday}' and terminal = ${terminal} and status = 1 and orderpaytype != -1"
 
+    result="'${yesterday}', a.ref, ${nox} AS neworxufei, '${terminal}',
+     count(distinct a.eid) as pageUv,
+     count(distinct b.userid) as payUv,
+     sum(coalesce(b.money, 0)) as income"
+
+    sql="${udf} select ${result} from (${sql2}) a left outer join (${sql3}) b on (a.uid = b.userid) group by a.ref, ${nox}"
+    print "${sql}"
+    hive -e "${sql}" >> ${file}
 }
 
 #入口方法
 function main {
     echo "开始计算${yesterday}日的站内渠道流量收入" > ${log}
+
+    #如果文件已经存在，则删除掉
+    delete_file ${file} >> ${log}
 
     echo "开始查询Http上报的PC端Ref流量" >> ${log}
     httpRef 112 >> ${log}
@@ -82,6 +106,14 @@ function main {
     echo "开始查询Http上报的M站Ref流量完成" >> ${log}
     httpRef 113 >> ${log}
     echo "查询Http上报的PC端Ref流量完成" >> ${log}
+
+    echo "开始查询JSSDK上报的PC端Ref流量" >> ${log}
+    jssdkRef 112 >> ${log}
+    echo "查询JSSDK上报的PC端Ref流量完成" >> ${log}
+
+    echo "开始查询JSSDK上报的M站Ref流量完成" >> ${log}
+    jssdkRef 113 >> ${log}
+    echo "查询JSSDK上报的PC端Ref流量完成" >> ${log}
 
     echo "开始导入数据到mysql中" >> ${log}
     delete ${db_ip} ${db_port} ${db_user} ${db_pass} ${db_name} "t_letv_channel_flow" "dt = '${yesterday}'" >> ${log}
